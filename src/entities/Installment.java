c9c66bbbdac0bf42e399a9f9df3dae40d5278e4c
Lucas Martins Arruda <lucas.martins@tigestao.com.br>
2023-02-16 13:53:35 -0300

    falta implementar o ContractService

--- src/entities/Installment.java
+++ src/entities/Installment.java
@@ -1,9 +1,10 @@
 package entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Installment {
-	
+	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private LocalDate date;
 	private Double amount;
 	
@@ -30,6 +31,13 @@ public class Installment {
 	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
+
+	@Override
+	public String toString() {
+		return date.format(fmt) + " - " + String.format("%.2f", amount);
+	}
+	
+	
 	
 	
 
